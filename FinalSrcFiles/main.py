a93187a96e01bd79514ddc921a4dc04419c52adb
Irtaza Sajid Qureshi <irtaza11@narval4.narval.calcul.quebec>
2024-04-09 07:07:31 -0400

    Add WideResNet and train to get all logs

--- FinalSrcFiles/main.py
+++ FinalSrcFiles/main.py
@@ -18,7 +18,8 @@ def main(args):
 
     model = get_model ( arch=args.arch, 
                         num_classes=(10 if args.dataset=='cifar10' else 100), 
-                        depth=args.depth, 
+                        depth=args.depth,
+                        widen_factor=args.widen_factor,
                         simam=args.simam )
     
     train(model, train_loader, test_loader, device, args.simam, writer=writer)
@@ -30,6 +31,7 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', type=str, default='cifar10', help='Dataset')
     parser.add_argument('--arch', type=str, default='mobilenetv2', help='Architecture')
     parser.add_argument('--depth', type=int, default=20, help='Depth of the network')
+    parser.add_argument('--widen_factor', type=int, default=10, help='Wide factor for WideResNet')
     parser.add_argument('--simam', action='store_true', help='Use SIMAM')
     args = parser.parse_args()
     
